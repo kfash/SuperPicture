54718e858d65134dab1df56d41124f79cd232820
kfash <rafi2001@mail.ru>
2019-10-06 15:41:38 +0300

    Update Pict.py
    У кота есть уши и морда. Прими с просто уши!!!!

--- Pict.py
+++ Pict.py
@@ -1,6 +1,7 @@
 from graph import *
-Sizex = 1000
-Sizey = 1000
+from tkinter import*
+Sizex = 2000
+Sizey = 2000
 windowSize(Sizex,Sizey)
 canvasSize(Sizex,Sizey)
 c=canvas()
@@ -25,6 +26,7 @@ def Window ():
 	rectangle(Sizex*39/70,Sizey*13/70,Sizex*47/70,Sizey*11/35)
 	
 def Cat ():
+	Ears()
 	c.create_oval(Sizex*27/70,Sizey*39/70,Sizex*9/14,  Sizey*22/35,fill="brown")
 	c.create_oval(Sizex/14,   Sizey*37/70,Sizex*3/7,   Sizey*5/7,fill="brown")
 	c.create_oval(Sizex/35,   Sizey*4/7,  Sizex*9/140, Sizey*47/70,fill="brown")
@@ -33,6 +35,7 @@ def Cat ():
 	c.create_oval(Sizex*23/70,Sizey*43/70,Sizex*3/7,   Sizey*5/7,fill="brown")
 	c.create_oval(Sizex*2/5,  Sizey*24/35,Sizex*61/140,Sizey*27/35,fill="brown")
 	Eyes()
+	Face()
 	
 	
 def Ravel ():
@@ -49,6 +52,23 @@ def Eyes ():
 	c.create_oval(Sizex*19/500,Sizey*281/500,Sizex*24/500,Sizey*299/500,fill="black")
 	c.create_oval(Sizex*51/500,Sizey*281/500,Sizex*56/500,Sizey*299/500,fill="black")
 	
+def Ears ():
+	penColor("black")
+	brushColor("brown")
+	polygon([(Sizex*90/1000, Sizey*530/1000),(Sizex*110/1000, Sizey*500/1000),(Sizex*130/1000, Sizey*550/1000),(Sizex*90/1000, Sizey*530/1000)])
+	polygon([(Sizex*60/1000, Sizey*530/1000),(Sizex*40/1000, Sizey*500/1000),(Sizex*20/1000, Sizey*550/1000),(Sizex*60/1000, Sizey*530/1000)])
+	brushColor("Sienna")
+	polygon([(Sizex*95/1000, Sizey*530/1000),(Sizex*110/1000, Sizey*508/1000),(Sizex*125/1000, Sizey*546/1000),(Sizex*95/1000, Sizey*530/1000)])
+	polygon([(Sizex*55/1000, Sizey*530/1000),(Sizex*40/1000, Sizey*508/1000),(Sizex*25/1000, Sizey*546/1000),(Sizex*55/1000, Sizey*530/1000)])
+
+def Face ():
+	brushColor ("LightSalmon")
+	polygon([(Sizex*65/1000, Sizey*600/1000),(Sizex*85/1000, Sizey*600/1000),(Sizex*75/1000, Sizey*620/1000),(Sizex*65/1000, Sizey*600/1000)])
+	polyline([(Sizex*75/1000,Sizey*620/1000),(Sizex*75/1000,Sizey*625/1000)])
+	c.create_arc(Sizex*75/1000,Sizey*620/1000,Sizex*65/1000,Sizey*630/1000,start=0,extent=-180,style=ARC,outline='black',width=1)
+	c.create_arc(Sizex*75/1000,Sizey*620/1000,Sizex*85/1000,Sizey*630/1000,start=0,extent=-180,style=ARC,outline='black',width=1)
+
+
 
 Back_Ground()
 Cat()
